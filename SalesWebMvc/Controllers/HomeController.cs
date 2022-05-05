a97dbcb05f9c3838bfde70f914ed14efcccc9c90
LuisEduardoGois <leoliveir81@gmail.com>
2022-05-05 11:07:49 -0300

    First controller and Razor tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales MVC Web Application";
+            ViewData["Author"] = "Luis Eduardo";
 
             return View();
         }
